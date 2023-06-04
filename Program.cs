566122e23126e175826f9c553bb5ead8aee851d2
hiba <hibaejaz06@gmail.com>
2023-06-04 21:20:50 +0300

    Item class created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create class `Item`, which has barcode, name and quantity, which are private. Each item should have unique barcode, and quantity can not be negative.
+internal class Program{
+    private static void Main(string[] args)
+        {
+    Item item=new Item();
+        }
+}
+
+class Item{
+    private int _barcode;
+    private string _name;
+    private int _quantity;
+
+    public int Quantity{
+       set{ if(_quantity>=0){
+        _quantity=_quantity;
+       }
+       }
+       get{
+        return _quantity;
+       }
+        
+    }
+}
+
+
